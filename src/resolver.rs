510565529439d9a9e196d17a83a9577d938825ca
Connor Winiarczyk <connorwiniarczyk@gmail.com>
2021-06-19 11:24:52 -0400

    wildcards working on right side of routes file

--- src/resolver.rs
+++ src/resolver.rs
@@ -4,17 +4,33 @@
 use tide::Response;
 use std::fs;
 use async_process::Command;
+
 use crate::config::Config;
+use crate::path_expression::*;
+
+use std::path::{PathBuf,Path};
+
+
+// pub enum PreProcessor {
+//     Exec (  )
+// }
+
+// pub struct Resource {
+//     path: PathExpr,
+
+// }
 
 #[derive(Debug, Clone)]
 pub enum Resolver {
-    File { path: String },
+    File { path: PathExpr },
     Exec { path: String },
 }
 
-pub fn resolve_file(file: &str, config: &Config) -> tide::Result {
-    let path = config.root.as_path().join(&file);
+pub fn resolve_file(file: &Path, config: &Config) -> tide::Result {
+    let path = config.root.as_path().join(&file.strip_prefix("/").unwrap());
+    println!("resolve path {:?}", path);
     let text = fs::read_to_string(path)?;
+    println!("{}", text);
     
     let response = Response::builder(200)
         .body(text)
@@ -36,15 +52,18 @@ pub async fn resolve_exec(file: &str, config: &Config) -> tide::Result {
 }
 
 impl Resolver {
-    pub async fn resolve(&self, config: &Config) -> tide::Result {
-        match self {
-            Self::File { path } => resolve_file(path, config),
+    pub async fn resolve(&self, path_match: PathMatch, config: &Config) -> tide::Result {
+       match self {
+            Self::File { path } => {
+                let resolved_path = path_match.to_path(&path);
+                resolve_file(&resolved_path, config)
+            },
             Self::Exec { path } => resolve_exec(path, config).await,
         }
     }
 
     pub fn file(path: &str) -> Self {
-        Self::File { path: path.to_string() }
+        Self::File { path: PathExpr::new(path) }
     }
 
     pub fn exec(path: &str) -> Self {
