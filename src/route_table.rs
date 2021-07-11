8a86f29e34c204d53655a2717576d3c1bd804113
connorwiniarczyk@gmail.com <connorwiniarczyk@gmail.com>
2021-07-11 01:34:29 +0000

    better, more verbose, logging
    FossilOrigin-Name: 5d6b522f837403bf290c8de56766ef7fa3d7af81d7a95e60f27f918d56929e98

--- src/route_table.rs
+++ src/route_table.rs
@@ -3,6 +3,8 @@
 use std::fmt;
 use std::path::Path;
 use tide::Request;
+use prettytable::{ Table, Row, Cell, row, cell };
+use itertools::Itertools;
 
 use crate::route_patterns::{ RequestPattern, ResourcePattern };
 use crate::options::RouteOption;
@@ -20,12 +22,14 @@ pub struct Route {
 
 
 impl Route {
-    pub fn resolve<'request>(&'request self, request: &'request Request<State>) -> Result<tide::Response, &'request str> {
+    pub async fn resolve<'request>(&'request self, request: &'request Request<State>, body: &'request Option<String>) -> Result<tide::Response, &'request str> {
         let request_match = self.request.compare(request)?;
         println!("\t found a matching route: {} --> {}", &self.request, &self.resource);
         println!("\t with wildcards: {:?}", &request_match.wildcards);
 
-        let mut response = ResponseGenerator::new(&request_match, &self, &request);
+        // read the request body if there is one
+
+        let mut response = ResponseGenerator::new(&request_match, &self, &request, body);
         for option in &self.options {
             response = option.apply(response);
         }
@@ -34,16 +38,13 @@ impl Route {
     }
 
     pub fn sanitize(mut self) -> Self {
-        let first_option = self.options.iter().next();
+        let has_access_type = self.options.iter()
+            .any(|x| x.name == "exec" || x.name == "read");
 
-        let needs_default = match first_option {
-            Some( RouteOption { processor, args, name } ) => {
-                !(name == "exec" || name == "read")
-            }
-            None => true,
-        };
+        if !has_access_type {
+            self.options.insert(0, RouteOption::new("read", vec![]));
+        }
 
-        self.options.insert(0, RouteOption::new("read", vec![]));
         return self
     }
 }
@@ -75,18 +76,32 @@ impl Default for RouteTable {
         let mut output = Self { table: vec![] };    
 
         // serve index.html as the root
-        output.add(parse("/ /index.html read header(content-type:text/html)").unwrap());
+        output.add(parse("/ /index.html read filetype(html)").unwrap());
 
         // serve javascript and css files from their own folders, use custom headers to make
         // things easier
-        output.add(parse("/scripts/* scripts/* read header(content-type:text/javascript)").unwrap());
-        output.add(parse("/styles/* /styles/*  read header(content-type:text/css)").unwrap());
+        output.add(parse("/scripts/* scripts/* filetype(js)").unwrap());
+        output.add(parse("/styles/* styles/*  read filetype(css)").unwrap());
 
         // serve general files, two directories deep
-        output.add(parse("/* /*  read").unwrap());
-        output.add(parse("/*/* /*/*  read").unwrap());
+        output.add(parse("/* *  read").unwrap());
+        output.add(parse("/*/* */*  read").unwrap());
 
         return output;
+    }
+}
+
+impl fmt::Display for RouteTable {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>)  -> fmt::Result {
+
+        let mut table = Table::new();
+        table.add_row(row![ "REQUEST", "RESOURCE", "OPTIONS" ]);
+        for row in self.iter() {
+            let options_str = row.options.iter().map(|x| x.to_string()).join(" ");
+            table.add_row(row![row.request, row.resource, options_str]);
+        }
+
+        write!(f, "{}", table)
     }
 }
 
