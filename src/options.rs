03285882fd4480ad4f312b08d473f72dc6e972dc
connorwiniarczyk@gmail.com <connorwiniarczyk@gmail.com>
2022-01-05 00:55:58 +0000

    dedicated processors module
    FossilOrigin-Name: 269d4ebfe515e29dd93379d4099de12767249da63f7c0b835580d05e80274850

--- src/options.rs
+++ src/options.rs
@@ -5,102 +5,28 @@ use itertools::Itertools;
 use crate::route_patterns::RequestMatch;
 use crate::route_table::{Route};
 
-type OptionFunc = for<'a> fn(ResponseGenerator<'a>, &Vec<Arg>) -> ResponseGenerator<'a>;
+use crate::processors::*;
+use crate::processors;
+
 
 #[derive(Clone)]
 pub struct RouteOption {
     pub args: Vec<Arg>,
-    pub func: OptionFunc,
-
-    pub func_name: String,
+    pub processor: Processor,
+    pub name: String, // the name of the processor
 }
 
 impl RouteOption {
-    pub fn new(func: &str, args: Vec<Arg>) -> Self {
-        let func_name = func.to_string();
-        let func = access_types::get_func(func);
-        Self { func, args, func_name }
+    pub fn new(processor: &str, args: Vec<Arg>) -> Self {
+        let name = processor.to_string();
+        let processor = processors::get(processor);
+        Self { processor, args, name }
     }
 
     pub fn apply<'request>(&self, response: ResponseGenerator<'request>) -> ResponseGenerator<'request> {
-        (self.func)(response, &self.args)
+        (self.processor)(response, &self.args)
     }
 }
-
-/// Declare an option using a javascript style arrow function syntax.
-/// Generates a function pointer that can be used in the `func` field of
-/// RouteOptions in the RouteTable. This is mainly useful as a way of
-/// abbreviating the very verbose function signature that `RouteOption` 
-/// requires.
-/// 
-/// Takes as arguments the name of the function, followed by an arrow function
-/// that performs some operation on a `ResponseGenerator`.
-///
-/// ### examples
-/// ```
-/// // Adds CORS headers to the response
-/// option!(cors (input) => {
-///     input.with_header("Access-Control-Allow-Origin", "*")
-/// });
-/// ```
-macro_rules! option {
-    ($name:ident, ($input:ident, $args:ident) => $func:block) => {
-        pub fn $name<'a>(mut $input: ResponseGenerator<'a>, $args: &Vec<Arg>) -> ResponseGenerator<'a> {
-            $func
-        }
-    };
-
-    ($name:ident, ($input:ident) => $func:block) => {
-        pub fn $name<'a>(mut $input: ResponseGenerator<'a>, _args: &Vec<Arg>) -> ResponseGenerator<'a> {
-            $func
-        }
-    };
-}
-
-
-pub mod access_types { 
-    use super::*;
-    use std::fs;
-
-
-    option!(exec, (response, args) => {
-        let path = response.route.resource.get_path(response.request_match);
-        let rendered_args: Vec<String> = args.iter()
-            .filter_map(|x| response.extract_data(x))
-            .collect();
-
-        let result = Command::new(&path).args(rendered_args).output().unwrap().stdout;
-        response.with_body(result)
-    });
-
-    option!(read, (input) => {
-        let path = input.route.resource.get_path(input.request_match);
-        input.with_body(fs::read(&path).unwrap_or_default()) 
-    });
-
-    option!(header, (input, args) => {
-        args.into_iter().fold(input, |response, arg| match arg {
-            Arg { name, value: Some(value) } => response.with_header(name, value),
-            Arg { name, value: None } => response,
-        })     
-    });
-
-    option!(cors, (input) => {
-        input.with_header("Access-Control-Allow-Origin", "*")
-    });
-
-
-    pub fn get_func(input: &str) -> OptionFunc {
-        match input {
-            "exec" => exec,
-            "read" => read,
-            "header" => header,
-            "cors" => cors,
-            _ => panic!(),
-        }
-    }
-}
-
 
 
 pub struct ResponseGenerator<'a> {
@@ -134,7 +60,7 @@ impl<'a> ResponseGenerator<'a> {
     }
 
     /// Sometimes, arguments reference data 
-    fn extract_data(&'a self, arg: &Arg) -> Option<String> {
+    pub fn extract_data(&'a self, arg: &Arg) -> Option<String> {
         let Arg { name, value } = arg;
         let output = match (name.as_str(), value) {
             ("query", Some(key)) => {
