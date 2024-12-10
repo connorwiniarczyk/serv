6375ca0c10a98d533364a65a67d52d8307bd5357
connor <>
2024-12-10 19:20:21 +0000

    better syntax for switch statements
    FossilOrigin-Name: 850a0b0ae5d2453aee5ce617ba57b6e677cf37a40963b61d462915cba63a36db

--- src/functions/mod.rs
+++ src/functions/mod.rs
@@ -171,7 +171,7 @@ fn choose(mut input: VecDeque<ServValue>, scope: &mut Stack) -> ServResult {
 }
 
 fn switch(mut input: VecDeque<ServValue>, scope: &mut Stack) -> ServResult {
-    let map = input.pop_front().unwrap_or(ServValue::None);
+    // let map = input.pop_front().unwrap_or(ServValue::None);
     let mut expr = input.pop_front().unwrap_or(ServValue::None);
     if let ServValue::Ref(label) = expr { expr = scope.get(label).unwrap() };
 
