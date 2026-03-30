8f784e42e4c8c7153c780e54ee87edcf257f6fbb
connor <>
2026-03-30 18:09:59 +0000

    added lib.rs to tracking
    FossilOrigin-Name: def418a1d5668ceb0de9376bd89271dce60b951441c968ddc5b0019dfb425a42

---
+++ src/lib.rs
@@ -0,0 +1,35 @@
+// mod parser;
+// mod datatypes;
+// use datatypes::*;
+// mod servlexer;
+// mod servparser;
+// mod parsetool;
+// mod engine;
+// mod error;
+// mod functions;
+// mod dictionary;
+// mod webserver;
+
+// use datatypes::module::ServModule;
+// use value::{ ServValue, ServFn };
+// use datatypes::reference::Address;
+// use error::ServError;
+// use dictionary::Label;
+// use clap::Parser;
+// use matchit::Router;
+// use dictionary::StackDictionary;
+// use dictionary::Stack;
+
+
+
+// mod servlexer;
+// mod datatypes;
+// use datatypes::*;
+// mod servlexer;
+// mod servparser;
+// mod parsetool;
+// mod engine;
+// mod error;
+// mod functions;
+// mod dictionary;
+// mod webserver;
